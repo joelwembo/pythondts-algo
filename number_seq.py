4265a620400596b677fbab97f04d57ab8b302913
Joel Wembo <joelotepawembo@gmail.com>
2021-04-12 09:24:16 -0400

    Create number_seq.py

---
+++ number_seq.py
@@ -0,0 +1,14 @@
+def next_number(s):
+    result = []
+    i = 0
+    while i < len(s):
+        count = 1
+        while i + 1 < len(s) and s[i] == s[i + 1]:
+            i += 1
+            count += 1
+        result.append(str(count) + s[i] )  
+        i += 1
+    return ''.join(result)
+
+seq = "1111"
+print(next_number(seq))
