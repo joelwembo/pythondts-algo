1d72f5b26b1157a3004bb7684dec215249220ccf
Joel Wembo <joelotepawembo@gmail.com>
2021-04-15 12:44:51 +0800

    Create Customer.py

---
+++ Customer.py
@@ -0,0 +1,14 @@
+class Customer:
+    def __init__(self, name, membership_type):
+        self.name = name
+        self.membership_type = membership_type
+        print("Customer created")
+
+    def update_memmbership(self, new_membership):
+        self.membership_type = new_membership
+           
+
+
+C = Customer('William', "Gold")
+
+print(C.name, C.membership_type)
