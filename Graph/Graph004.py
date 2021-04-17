96998e9110937dce43818c32e5fb336097b9e8b6
Joel Wembo <joelotepawembo@gmail.com>
2021-04-17 15:29:15 +0800

    Update Graph004.py

--- Graph/Graph004.py
+++ Graph/Graph004.py
@@ -43,10 +43,10 @@ if __name__ == "__main__":
     V = 5
     graph = Graph(V)
     graph.add_edge(0, 1)
-    graph.add_edge(0, 4)
+    graph.add_edge(0, 5)
     graph.add_edge(1, 2)
-    graph.add_edge(1, 3)
     graph.add_edge(1, 4)
+    graph.add_edge(1, 5)
     graph.add_edge(2, 3)
     graph.add_edge(3, 4)
  
