4dbc38c805d6975f19f21df7e2bd7cf4112ab0f9
Joel Wembo <joelotepawembo@gmail.com>
2021-04-23 21:27:27 +0800

    Update amortization.py

--- amortization.py
+++ amortization.py
@@ -1,9 +1,10 @@
 import sys
 
 data = []
-for k in range(100):
+for k in range(200):
     a = len(data).
     b = sys.getsizeof(data)
     print('Length: {0:3d}; Size in bytes: {1:4d}'.format(a,b))
     data.append(None)
-//
+    
+    
