4b74712eaaea88228ef99543acd7a57cadc3f7de
Joel Wembo <joelotepawembo@gmail.com>
2021-05-18 22:41:59 +0800

    factorial

--- dts_example1.py
+++ dts_example1.py
@@ -1,3 +1,4 @@
+#Question: Write a program which will find all such numbers which are divisible by 7 but are not a multiple of 5, between 2000 and 3200 (both included). The numbers obtained should be printed in a comma-separated sequence on a single line.
 
 
 l = []
@@ -6,7 +7,21 @@ for i in range(2000, 3201):
         l.append(str(i))
 
            
+#print(' ,'.join(l))      test   
 
-print(' ,'.join(l))
+# Factorial problem
+
+def factorial(n): 
+    if n == 0: 
+        return 1
+    return n * factorial(n - 1)  
+
+y = int(input("Please enter an non null integer"))
+
+print(',' , factorial(y) ) 
+
+
+
+
 
 
