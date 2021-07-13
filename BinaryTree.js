0950aac6cff17a780ccc02e3c5e7f4bda9449663
Joel Wembo <joelotepawembo@gmail.com>
2021-07-13 15:14:02 +0800

    Add files via upload

---
+++ BinaryTree.js
@@ -0,0 +1,47 @@
+class Node {
+    constructor(val) {
+        this.val = val;
+        this.right = null;
+        this.left = null;
+        this.count = 0;
+    };
+};
+
+class BST {
+    constructor() {
+        this.root = null;
+    }
+    create(val) {
+        const newNode = new Node(val);
+        if (!this.root) {
+            this.root = newNode;
+            return this;
+        };
+        let current = this.root;
+
+        const addSide = side => {
+            if (!current[side]) {
+                current[side] = newNode;
+                return this;
+            };
+            current = current[side];
+        };
+
+        while (true) {
+            if (val === current.val) {
+                current.count++;
+                return this;
+            };
+            if (val < current.val) addSide('left');
+            else addSide('right');
+        };
+    };
+};
+
+let tree = new BST();
+tree.addSide(10);
+tree.addSide(4);
+tree.addSide(4);
+tree.addSide(12);
+tree.addSide(2);
+console.log(tree);
